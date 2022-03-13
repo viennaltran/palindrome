b42ac48782a5366af71d62df1239f10c050431f6
Vienna Tran <vitran@mills.edu>
2022-03-12 21:23:47 -0800

    Add more tests

--- src/edu/mills/cs180/PalindromeCheckerTest.java
+++ src/edu/mills/cs180/PalindromeCheckerTest.java
@@ -1,13 +1,28 @@
 package edu.mills.cs180;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.*;
 
 class PalindromeCheckerTest {
 
     @Test
     void isPalindrome_True_EmptyString() {
         assertTrue(PalindromeChecker.isPalindrome(""));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"AMA", "racecar", "x"})
+    void isPalindrome(String s) {
+        assertTrue(PalindromeChecker.isPalindrome(s));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Java", "abc", "spatula"})
+    void isNotPalindrome(String s) {
+        assertFalse(PalindromeChecker.isPalindrome(s));
     }
 
 }
